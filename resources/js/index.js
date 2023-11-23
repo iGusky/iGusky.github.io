5d930ffa3d5895aba5a2b937d59a5aaa3dfb948c
Gustavo Hernández <gustavohernandez.dev@gmail.com>
2023-11-23 11:51:13 -0600

    v1.1 design: Agrega estilos iniciales

---
+++ resources/js/index.js
@@ -0,0 +1,9 @@
+function scrollToAboutMe() {
+  document.querySelector('#aboutme').scrollIntoView({behavior: 'smooth'});
+}
+function scrollToJob() {
+  document.querySelector('#job').scrollIntoView({behavior: 'smooth'});
+}
+function scrollToContact() {
+  document.querySelector('#contact').scrollIntoView({behavior: 'smooth'});
+}
